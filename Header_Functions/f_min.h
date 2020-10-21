9e9d60ce646a72d2c60fa2339546b23d61218d98
Kamel1 <ren.cerbah@gmail.com>
2020-10-21 10:55:30 +0100

    Add the min functions plus some changes to the previous files

---
+++ Header_Functions/f_min.h
@@ -0,0 +1,5 @@
+// Min function of two numbers
+//made with shorthand if
+float f_min(float n1,float n2){
+    return (n1<n2)? n1 :n2 ;
+}
--- main.c
+++ main.c
@@ -1,15 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-//adding the f_max function
-#include "Header Functions/max.h"
+//adding the f_max function header file
+#include "Header_Functions/f_max.h"
+//adding the f_max function header file
+#include "Header_Functions/f_min.h"
 
 int main()
 {       // testing the max function
 
         float n1,n2;
         printf("Enter two numbers :\n");
+
+        //taking in two numbers to test the min and max functions
         scanf("%f%f",&n1,&n2);
+
+        //testing if the max function works
          printf("\n Max is : %.2f \t",f_max(n1,n2));
+
+        //testing if the max function works
+         printf("\n Min is : %.2f \t",f_min(n1,n2));
     return 0;
 }
