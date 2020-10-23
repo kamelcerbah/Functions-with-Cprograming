1e45277d514e849554fb159c0e1912b3ab5faf48
Kamel1 <ren.cerbah@gmail.com>
2020-10-23 12:10:50 +0100

    integrated the f_add function with the source code

--- main.c
+++ main.c
@@ -5,6 +5,8 @@
 #include "Header_Functions/f_max.h"
 //adding the f_max function header file
 #include "Header_Functions/f_min.h"
+//adding the f_add function header file
+#include "Header_Functions/f_add.h"
 
 int main()
 {       // testing the max function
@@ -20,5 +22,10 @@ int main()
 
         //testing if the max function works
          printf("\n Min is : %.2f \t",f_min(n1,n2));
+
+         //testing if the add function works
+         printf("\n %.2f + %.2f is = %.2f \t",n1,n2,f_add(n1,n2));
+
+
     return 0;
 }
